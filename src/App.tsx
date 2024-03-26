3cb194e76d00bebd8ad9efbfbc133656b34dfeef
Mitchell Keo <mitchellnkeo@gmail.com>
2024-03-26 14:48:08 -0700

    Created Hangman components.

---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import { HangmanDrawing } from './HangmanDrawing'
+import { HangmanWord } from './HangmanWord'
+import { Keyboard } from './Keyboard'
+import words from "./wordList.json"
+
+function App() {
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)]
+  })
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([])
+
+  const incorrectLetters = guessedLetters.filter(
+    letter => !wordToGuess.includes(letter))
+
+  console.log(wordToGuess)
+  
+  return (
+    <div style={{
+      maxWidth:"800px",
+      display: "flex",
+      flexDirection:"column",
+      gap:"2rem",
+      margin: "0 auto",
+      alignItems: "center"
+    }}
+    >
+    <div style={{ fontSize: "2rem", textAlign: "center" }}> Lose Win </div>
+    <HangmanDrawing numberOfGuesses={incorrectLetters.length}/>
+    <HangmanWord />
+    <div style ={{ alignSelf: "stretch"}}>
+    <Keyboard />
+    </div>
+    </div>
+  )
+}
+
+export default App
